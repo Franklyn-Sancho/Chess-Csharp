5923ce408174cd209f6ad244a5c15036a6ce91e7
Franklyn-sancho <franklyn.hs@gmail.com>
2021-10-11 11:43:48 -0300

    tratanto erros com excecao

--- tabuleiro/Tabuleiro.cs
+++ tabuleiro/Tabuleiro.cs
@@ -15,9 +15,34 @@ namespace tabuleiro {
             return pieces[linha, coluna];
         }
 
+        public Piece piece(Position pos) {
+            return pieces[pos.linha, pos.coluna];
+        }
+
+        public bool existPiece(Position pos) {
+            acceptPosition(pos);
+            return piece(pos) != null;
+        }
+
         public void putPiece(piece p, position pos) {
+            if(existPiece(pos)) {
+                throw new TabuleiroException("Já existe uma peça nessa posição");
+            }
             pieces[pos.linha, pos.coluna] = p;
             p.position = pos;
+        }
+
+        public bool validPosition(Position pos) {
+            if(pos.linha <0 || pos.linha >=linhas || pos.coluna<0 || pos.coluna>=colunas) {
+                return false;
+            }
+            return true;
+        }
+
+        public void acceptPosition(Position pos) {
+            if(!validPosition(pos)) {
+                throw new TabuleiroException("Posição Inválida!");
+            }
         }
     }
 }
---
+++ tabuleiro/TabuleiroException.cs
@@ -0,0 +1,7 @@
+using System;
+
+namespace tabuleiro {
+    class TabuleiroException: Exception {
+
+    } 
+}
