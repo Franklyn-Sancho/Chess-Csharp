fc24d620ef7398bbe25b824fd02f435f9c4b8e5d
Franklyn Sancho <69439514+Franklyn-Sancho@users.noreply.github.com>
2021-10-21 14:00:48 -0300

    Update Peao.cs

--- xadrez/Peao.cs
+++ xadrez/Peao.cs
@@ -12,7 +12,7 @@ namespace xadrez
         }
         public override string ToString()
         {
-            return "R";
+            return "P";
         }
 
         //O método verifica duas coisas:
